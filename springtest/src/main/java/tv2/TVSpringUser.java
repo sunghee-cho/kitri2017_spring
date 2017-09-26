04a4af9024e1e532f3eee6dbd1d18d67f59c067e
KITRIGAME <KITRIGAME@KITRIGAME-PC>
2017-09-26 14:28:38 +0900

    수정7

--- springtest/src/main/java/tv2/TVSpringUser.java
+++ springtest/src/main/java/tv2/TVSpringUser.java
@@ -60,6 +60,7 @@ class LgTV implements TV{
 // spring : factory method 패턴
 public class TVSpringUser {
 public static void main(String[] args) {
+	System.out.println("github_원본1");
 	ApplicationContext factory
 	= new ClassPathXmlApplicationContext
 	("tv2/tv.xml");
